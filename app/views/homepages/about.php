bf1a3e73a5716c8598872ce37852a66508beddfc
Sander Dijkxhoorn <15117158+sanderDijkxhoorn@users.noreply.github.com>
2022-11-19 19:00:16 +0100

    Added the overige informatie (about)

---
+++ app/views/homepages/about.php
@@ -0,0 +1,74 @@
+<link rel="stylesheet" href="/css/overige-info.css">
+
+<!--Here is the slideshow and information-->
+<div class="wrapper2">
+    <form class="form2">
+        <span id="item-1"></span>
+        <span id="item-2"></span>
+        <span id="item-3"></span>
+        <div class="carousel-item item-1">
+            <h2>Over MBO Utrecht</h2>
+            <div class="background">
+                <img src="/img/background-home.png" width="50%" height="0%" alt="">
+            </div><br>
+            <p>MBO Utrecht is een kleinschalige, interconfessionele onderwijsinstelling die doelmatig onderwijs aanbiedt met
+                focus op de hoogste niveaus van het mbo. We bieden met ca. 500 medewerkers ruim 50 mbo-opleidingen aan
+                ongeveer 5.000 studenten, in uiteenlopende de domeinen van het mbo-onderwijs.</p> <br>
+            <p>MBO Utrecht wil jou helpen in de ontwikkeling van student tot zelfbewuste professional. Wij geven jou meer
+                dan een opleiding en leren je meer dan een vak. Er is veel aandacht voor de ontwikkeling van talent,
+                arbeidsvaardigheden, ondernemend gedrag, maatschappelijke betrokkenheid, respect voor elkaar en burgerschap.
+            </p>
+            <a class="arrow arrow-prev" href="#item-3"></a>
+            <a class="arrow arrow-next" href="#item-2"></a>
+        </div>
+
+        <div class="carousel-item item-2">
+            <h2>Hoe werkt de school?</h2> <br>
+            <div class="background">
+                <img src="/img/Daltonlaan-opti.jpg" width="50%" height="0%" alt="">
+            </div>
+            <h3>1. Kleinschalige school & betrokken docenten</h3> <br>
+            Bij MBO Utrecht kun je op goed bereikbare locaties wel 50 verschillende opleidingen volgen. Wij zijn een
+            kleinschalige school waar jij je snel thuis zult voelen. Dat komt door onze betrokken docenten die niet alleen
+            aandacht hebben voor jou als persoon, maar ook voor de manier waarop jij leert.
+            <br> <br>
+            <h3>2. Ruimte voor maatwerk</h3> <br>
+            Er is veel ruimte voor maatwerk. We hebben bijvoorbeeld veel doorstroom- en versnellingsmogelijkheden. Maar er
+            is ook ondersteuning wanneer dat nodig is.
+            <br> <br>
+            <h3>3. Straks een goed opgeleide professional</h3> <br>
+            Bij ons word je niet alleen opgeleid voor een vak. We vinden het belangrijk dat je naast je opleiding ook
+            vaardigheden leert die je helpen in deze wereld die continu verandert. Zo ben jij straks een goed opgeleide
+            professional die klaar is om in de praktijk aan de slag te gaan.</p>
+            <a class="arrow arrow-prev" href="#item-1"></a>
+            <a class="arrow arrow-next" href="#item-3"></a>
+        </div>
+
+        <div class="carousel-item item-3">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d156940.37104759057!2d5.104826!3d52.076211!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c668a291ed7f45%3A0x9b18dabb8687ad2!2sMBO%20Utrecht%2C%20Daltonlaan%2C%20Utrecht!5e0!3m2!1snl!2snl!4v1663183115242!5m2!1snl!2snl" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <a class="arrow arrow-prev" href="#item-2"></a>
+            <a class="arrow arrow-next" href="#item-1"></a>
+        </div>
+</div>
+</form>
+</div>
+
+<!--The contact form-->
+<div class="wrapper">
+    <form class="form">
+        <div class="pageTitle title">Contact</div>
+        <input type="text" class="name formEntry" placeholder="Naam" required />
+        <input type="text" class="surname formEntry" placeholder="Achternaam" required />
+        <input type="email" class="email formEntry" placeholder="Email" required />
+        <input type="tel" class="phone number formEntry" placeholder="Telefoonnummer" required />
+        <textarea class="message formEntry" placeholder="Uw bericht" required></textarea>
+
+        <button class="submit formEntry" onclick="thanks()">Verzenden</button>
+    </form>
+</div>
+
+<div class="register"> <br>
+    <h1>Mis het niet en schrijf je nu in!</h1> <br>
+    <a href="register.php"> <button class="button">Registreer</button></a>
+    <br>
+</div>
