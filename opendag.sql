7a9d32b32a2e82714a22c8cc9c124de7355739ad
Sander Dijkxhoorn <15117158+sanderDijkxhoorn@users.noreply.github.com>
2022-11-19 17:14:56 +0100

    Remove some test records

--- opendag.sql
+++ opendag.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 19, 2022 at 04:09 PM
+-- Generation Time: Nov 19, 2022 at 04:14 PM
 -- Server version: 5.7.34
 -- PHP Version: 8.0.8
 
@@ -51,12 +51,9 @@ CREATE TABLE `antwoorden` (
 --
 
 INSERT INTO `antwoorden` (`id`, `enqueteId`, `answeredById`, `antwoord1`, `antwoord2`, `antwoord3`, `antwoord4`, `antwoord5`, `antwoord6`, `antwoord7`, `antwoord8`, `antwoord9`, `antwoord10`) VALUES
-(1, 1, 2, 'Cool antwoord 1', 'Cool antwoord 2', 'Cool antwoord 3', 'Cool antwoord 4', 'Cool antwoord 5', 'Cool antwoord 6', 'Cool antwoord 7', 'Cool antwoord 8', 'Cool antwoord 9', 'Cool antwoord 10'),
-(2, 2, 2, 'Antwoord 1', 'Antwoord 2', 'Antwoord 3', 'Antwoord 4', 'Antwoord 5', 'Antwoord 6', NULL, NULL, NULL, NULL),
-(3, 1, 2, 'antwoord 1', 'antwoord 2', 'antwoord 3', 'antwoord 4', 'antwoord 5', 'antwoord 6', 'antwoord 7', 'antwoord 8', 'antwoord 9', 'antwoord 10'),
-(12, 1, 3, 'Echt', 'Hele', 'Mooie', 'Antwoorden', 'Speciaal', 'Gemaakt', 'Voor', 'Jouw', 'Alsjeblieft', 'Meneer :)'),
-(17, 1, 3, 'test', 'f', 'f', 'f', 'ff', 'f', 'f', 'f', 'f', 'f'),
-(18, 19, 3, 'Nigger', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(1, 1, 1, 'Cool antwoord 1', 'Cool antwoord 2', 'Cool antwoord 3', 'Cool antwoord 4', 'Cool antwoord 5', 'Cool antwoord 6', 'Cool antwoord 7', 'Cool antwoord 8', 'Cool antwoord 9', 'Cool antwoord 10'),
+(2, 1, 2, 'antwoord 1', 'antwoord 2', 'antwoord 3', 'antwoord 4', 'antwoord 5', 'antwoord 6', 'antwoord 7', 'antwoord 8', 'antwoord 9', 'antwoord 10'),
+(3, 1, 2, 'Echt', 'Hele', 'Mooie', 'Antwoorden', 'Speciaal', 'Gemaakt', 'Voor', 'Jouw', 'Alsjeblieft', 'Meneer :)');
 
 -- --------------------------------------------------------
 
@@ -76,12 +73,9 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `name`, `email`, `comment`) VALUES
-(1, 'Testing', 'email@email.com', 'Hello Everyone.'),
-(2, 'abc', 'admin@localhost.com', '\'\r\n'),
-(3, 'Musab Bin Abdul Bari', 'purecodingofficial@gmail.com', 'Hello Everyone. How are you. Please Subscribe Pure Coding YouTube Channel.'),
-(4, 'ABC', 'abc@gmail.com', 'ABC'),
-(5, 'Test', 'persoon@mail', 'lol'),
-(6, 'dad', 'sadd@dada.d', 'dadwad\r\n');
+(1, 'ABC', 'abc@gmail.com', 'ABC'),
+(2, 'Test', 'persoon@mail', 'lol'),
+(3, 'dad', 'sadd@dada.d', 'dadwad\r\n');
 
 -- --------------------------------------------------------
 
@@ -109,18 +103,7 @@ CREATE TABLE `enquetes` (
 
 INSERT INTO `enquetes` (`id`, `Vraag1`, `Vraag2`, `Vraag3`, `Vraag4`, `Vraag5`, `Vraag6`, `Vraag7`, `Vraag8`, `Vraag9`, `Vraag10`) VALUES
 (1, 'Mooie vraag 1', 'Mooie vraag 2', 'Mooie vraag 3', 'Mooie vraag 4', 'Mooie vraag 5', 'Mooie vraag 6', 'Mooie vraag 7', 'Mooie vraag 8', 'Mooie vraag 9', 'Mooie vraag 10'),
-(2, 'Vraag 1', 'Vraag 2', 'Vraag 3', 'Vraag 4', 'Vraag 5', 'Vraag 6', NULL, NULL, NULL, NULL),
-(9, 'tetlal', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(10, 'ffhfsfuuh', 'h', 'uh', 'uho', NULL, NULL, NULL, NULL, NULL, NULL),
-(11, '3i3ijs', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(12, 'jhvjvj', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(13, 'oOajas', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(14, 'Lmao', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(15, 'Gaaf', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(16, 'd', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(17, 'dadawdawdwa', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(18, 'Mooie vraaggggg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(19, 'g', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(2, 'Vraag 1', 'Vraag 2', 'Vraag 3', 'Vraag 4', 'Vraag 5', 'Vraag 6', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -142,8 +125,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`) VALUES
 (1, 'admin', 'admin@admin.admin', '$2y$10$uuHAyBNah8U9JRsLQhW58.KPqNSRzePQa5BAi8BXXAG5kLB9IzYlW', 'admin'),
-(4, 'nietadmin', 'nietadmin@nietadmin.nietadmin', '$2y$10$wahE6iR9uAAbFQZCby90pOAce4Mt6szTQdx5jHGnvSKIgyFLwUn6S', 'user'),
-(9, 'dwuao', 'awdij@adij.adwij', '$2y$10$WjRbVNcAq1W.ZY1.ZV4ow.nxxkEDzDA.gCCIayZvDdiVmK8Wx8J9O', 'user');
+(2, 'nietadmin', 'nietadmin@nietadmin.nietadmin', '$2y$10$wahE6iR9uAAbFQZCby90pOAce4Mt6szTQdx5jHGnvSKIgyFLwUn6S', 'user');
 
 --
 -- Indexes for dumped tables
