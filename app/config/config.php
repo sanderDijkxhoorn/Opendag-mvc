b7df4f7dd58028e673cf6ee3188ec56c010e8901
Saad332556 <332556@student.mboutrecht.nl>
2022-11-20 20:25:31 +0100

    style user overview

--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,11 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_PASS', '');
 define('DB_NAME', 'opendag');
 
 define('APPROOT', dirname(dirname(__FILE__)));
 
-define('URLROOT', 'http://localhost:8888');
+define('URLROOT', 'http://opendag-mvc2.0.com/');
 
 define('SITENAME', 'Opendag MVC');
--- app/views/accounts/index.php
+++ app/views/accounts/index.php
@@ -3,7 +3,7 @@
 <?php
 // Show deleteStatus if exists as alert
 if (isset($data['deleteStatus'])) {
-  echo '<div class="alert alert-success" role="alert">' . $data['deleteStatus'] . '</div>';
+  echo "<p class='success'>" . $data['deleteStatus'] . "</p>";
 }
 
 // If $data['error'] is set, show the error message
@@ -14,8 +14,9 @@ if (isset($data['error'])) {
 }
 ?>
 
-<table>
+<table id="read">
   <thead>
+    <th>Id</th>
     <th>Username</th>
     <th>Email</th>
     <th>Role</th>
@@ -23,19 +24,21 @@ if (isset($data['error'])) {
     <th>delete</th>
   </thead>
   <tbody>
-    <?php
-    // Make the data available in the view
-    $rows = '';
-    foreach ($data['accounts'] as $value) {
-      echo "
-          <tr>
-            <td>" . htmlentities($value->username, ENT_QUOTES, 'ISO-8859-1') . "</td>
-            <td>" . htmlentities($value->email, ENT_QUOTES, 'ISO-8859-1') . "</td>
-            <td>" . htmlentities($value->role, ENT_QUOTES, 'ISO-8859-1') . "</td>
-            <td><a href='" . URLROOT . "/accounts/update/$value->id'>update</a></td>
-            <td><a href='" . URLROOT . "/accounts/delete/$value->id'>delete</a></td>
-          </tr>";
-    }
-    ?>
+
+  <?php
+  // Make the data available in the view
+  $rows = '';
+  foreach ($data['accounts'] as $value) {
+    echo "
+      <tr>
+        <td>" . htmlentities($value->id, ENT_QUOTES, 'ISO-8859-1') . "</td>
+        <td>" . htmlentities($value->username, ENT_QUOTES, 'ISO-8859-1') . "</td>
+        <td>" . htmlentities($value->email, ENT_QUOTES, 'ISO-8859-1') . "</td>
+        <td>" . htmlentities($value->role, ENT_QUOTES, 'ISO-8859-1') . "</td>
+        <td><a href='" . URLROOT . "/accounts/update/$value->id'><button class='button'>update</button></a></td>
+        <td><a href='" . URLROOT . "/accounts/delete/$value->id'><button class='button'>delete</button></a></td>
+      </tr>";
+  }
+  ?>
   </tbody>
 </table>
