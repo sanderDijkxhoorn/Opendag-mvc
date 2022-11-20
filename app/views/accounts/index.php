5b7a3286f84a56513b0e033607fa53680453229e
Saad332556 <332556@student.mboutrecht.nl>
2022-11-20 23:31:26 +0100

    user overview style

--- app/views/accounts/index.php
+++ app/views/accounts/index.php
@@ -1,4 +1,8 @@
+<!-- Styling -->
 <link rel="stylesheet" href="/css/accounts.css">
+
+<!-- Icons -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <?php
 // Show deleteStatus if exists as alert
@@ -17,11 +21,10 @@ if (isset($data['error'])) {
 <table id="read">
   <thead>
     <th>Id</th>
-    <th>Username</th>
+    <th>Gebruikersnaam</th>
     <th>Email</th>
-    <th>Role</th>
-    <th>update</th>
-    <th>delete</th>
+    <th>Rol</th>
+    <th>Acties</th>
   </thead>
   <tbody>
     <?php
@@ -34,8 +37,14 @@ if (isset($data['error'])) {
         <td>" . htmlentities($value->username, ENT_QUOTES, 'ISO-8859-1') . "</td>
         <td>" . htmlentities($value->email, ENT_QUOTES, 'ISO-8859-1') . "</td>
         <td>" . htmlentities($value->role, ENT_QUOTES, 'ISO-8859-1') . "</td>
-        <td><a href='" . URLROOT . "/accounts/update/$value->id'><button class='button'>update</button></a></td>
-        <td><a href='" . URLROOT . "/accounts/delete/$value->id'><button class='button'>delete</button></a></td>
+        <td>
+            <div class='dropdown'>
+              <button class='dropbtn'><i class='fa fa-bolt'></i> Acties</button>
+              <div class='dropdown-content'>
+                <a href='" . URLROOT . "/accounts/update/$value->id'>Update</a>
+                <a href='" . URLROOT . "/accounts/delete/$value->id'>Verwijder</a>
+              </div>
+            </div></td>
       </tr>";
     }
     ?>
--- app/views/accounts/update.php
+++ app/views/accounts/update.php
@@ -1,33 +1,29 @@
 <link rel="stylesheet" href="/css/accounts.css">
 
+<div class="header">
+    <h2>Update gebruiker</h2>
+</div>
+
 <form action="/accounts/update" method="POST">
-    <table>
-        <tbody>
-            <input type="hidden" name="id" value="<?= $data['row']->id; ?>">
-            <tr>
-                <td>
-                    <input type="text" name="username" value="<?= $data["row"]->username ?>">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="text" name="email" value="<?= $data["row"]->email ?>">
-                </td>
-            </tr>
-            <tr>
-                <!-- Select menu between admin and user -->
-                <td>
-                    <select name="role">
-                        <option value="user" <?= $data["row"]->role == "user" ? "selected" : "" ?>>User</option>
-                        <option value="admin" <?= $data["row"]->role == "admin" ? "selected" : "" ?>>Admin</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="submit" value="Bewerken">
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="input-group">
+        <input type="hidden" name="id" value="<?= $data['row']->id; ?>">
+    </div>
+    <div class="input-group">
+        <label>Gebruikersnaam</label>
+        <input type="text" name="username" value="<?= $data["row"]->username ?>">
+    </div>
+    <div class="input-group">
+        <label>Email</label>
+        <input type="text" name="email" value="<?= $data["row"]->email ?>">
+    </div>
+    <div class="input-group">
+        <label>Rollen</label>
+        <select name="role">
+            <option value="user" <?= $data["row"]->role == "user" ? "selected" : "" ?>>User</option>
+            <option value="admin" <?= $data["row"]->role == "admin" ? "selected" : "" ?>>Admin</option>
+        </select>
+    </div>
+    <div class="input-group">
+        <button type="submit" class="btn">Bewerken</button>
+    </div>
 </form>
