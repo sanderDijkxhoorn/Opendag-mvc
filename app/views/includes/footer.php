743f7ae4e892b0f2f5c830deaaa7ef530e97b81e
Sander Dijkxhoorn <15117158+sanderDijkxhoorn@users.noreply.github.com>
2022-12-20 11:41:21 +0100

    Remove other links

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,11 +1,6 @@
 <footer class="sticky-footer">
     <ul>
         <li><a href="/index.php">Home</a></li>
-        <li><a href="/overige-info.php">Overige Informatie</a></li>
-        <li><a href="/enquete.php">Enquête</a></li>
-        <li><a href="/overview.php">Overview</a></li>
-        <li><a href="/overview.php">Projecten</a></li>
-        <li><a href="/overview.php">Comment</a></li>
         <li><a href="/overview.php">Login</a></li>
         <li><a href="/overview.php">Register</a></li>
     </ul>
